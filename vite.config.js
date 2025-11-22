a37e45f838e12832543705be6070b81a0b8291bf
Jose Guillermo Vasquez <jguiller412@gmail.com>
2025-11-22 16:27:33 -0500

    configuración para TuAvocadonet2.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TuAvocadoWeb/",  // <--- ¡AGREGA ESTA LÍNEA!
+  base: "/TuAvocadonet2.0/",
 })
